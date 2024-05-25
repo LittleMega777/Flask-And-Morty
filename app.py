d377d071ad6f2693be66c078481ce24205f6751d
LittleMega777 <pierrecouto02@gmail.com>
2024-05-25 15:37:29 -0300

    feat(style.css, index.html, app.py)- melhoria no back-engine

--- app.py
+++ app.py
@@ -6,16 +6,18 @@ app = Flask(__name__)
 
 def sorteio_personagem():
   resposta = requests.get(f"https://rickandmortyapi.com/api/character/{randint(1, 826)}", verify=False)
-  return resposta.json()["image"]
+  return resposta.json()
+
+personagem = sorteio_personagem()
 
 @app.route('/')
 def hello():
-    return render_template('index.html', url=sorteio_personagem())
+    return render_template('index.html', url=personagem['image'], name=personagem['name'], status=personagem['status'], especie=personagem['species'])
 
 @app.route('/', methods=['POST'])
 def sorteio():
-   button_value = request.form['button']
-   return render_template('index.html', url=sorteio_personagem())
+   personagem = sorteio_personagem()
+   return render_template('index.html', url=personagem['image'], name=personagem['name'], status=personagem['status'], especie=personagem['species'])
 
 if __name__ == '__main__':
     app.run(debug=True)
